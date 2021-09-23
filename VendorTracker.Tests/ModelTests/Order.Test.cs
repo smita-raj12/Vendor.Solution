d230dc79f0f738e3c4e889034a6fefb75cfcee88
smita Raj <smita.raj12@gmail.com>
2021-09-23 09:59:33 -0700

    add test files

---
+++ VendorTracker.Tests/ModelTests/Order.Test.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorTracker.Models;
+using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test1","test2","test3","test4");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}    
